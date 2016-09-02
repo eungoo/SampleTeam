2b51ed78fc3f052add677606d1bad883025ca54c
1-718-8 <1-718-8@PRO-717-8-PC>
2016-09-02 16:03:17 +0900

    team member 2

--- TestTestTest/src/Hello.java
+++ TestTestTest/src/Hello.java
@@ -1,9 +1,8 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("나는 팀장이다!!");
-		
-		
-		System.out.println("나는 팀원인데..");
+		System.out.println("나는 팀장이다!!" );
+			
+		System.out.println("나는 팀원인데.. 2");
 	}
 }
